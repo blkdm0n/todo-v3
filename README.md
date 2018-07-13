8be4a777fab5ace88bf142aae692584c70a14dce
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-07-13 15:30:11 -0700

    completed requirement 5 and refactored code to use the this keyword

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Basic Requirements:
 [X] It should store the todos array on an object
 [X] It should have a displayTodos method
 [X] It should have an addTodo method
-[] It should have a changeTodo method
-[] It should have a deleteTodo method
+[X] It should have a changeTodo method
+[X] It should have a deleteTodo method
 
--- index.js
+++ index.js
@@ -4,13 +4,33 @@
 
 //REQ 3 - It should have an addTodo method
 
+//REQ 4 - It should have a changeTodo method
+
+//REQ 5 - It should have a deleteTodo method
+
 var todoList = {
   todos: [`item1`, `item2`, `item3`],
   displayTodos: function () {
-    console.log(todoList.todos);
+    console.log(this.todos);
   },
   addTodo: function (todo) {
-    todoList.todos.push(todo);
-  } 
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function (oldTodo, newTodo) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i] === oldTodo) {
+        this.todos[i] = newTodo;
+      } 
+    }
+    this.displayTodos();
+  },
+  deleteTodo: function (deletedTodo) {
+    for (let i = 0; i < todoList.todos.length; i++) {
+      if (this.todos[i] === deletedTodo) {
+         this.todos.splice(i, 1);
+      } 
+    }
+    this.displayTodos()
+  }
 }
-
